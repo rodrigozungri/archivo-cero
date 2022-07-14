bcca2945ef240c922e249e31b8567894ad72e3d0
Rodrigo Zungri <biozungri@gmail.com>
2022-07-14 20:05:20 -0300

    add text in home

--- form/send.php
+++ form/send.php
@@ -1,8 +1,27 @@
 <?php
 
+// Conexion form > server (name)
 $name = $_POST['act_name'];
 $email = $_POST['act_email'];
 $phone = $_POST['act_tel'];
-$message = $_POST['email'];
+$message = $_POST['act_message'];
+
+// Texto plano
+$header .= "Content-Type: text/plain";
+
+// Enviar datos ingresados en el form al mail
+$message = "Este mensaje fue enviado por " . $name . ",\r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Mensaje: " . $_POST['act_message'] . " \r\n";
+$message .= "Enviado el " . date('d/m/Y', time());
+
+$for = 'contacto@rodrigozungri.com'; // Email de destino
+$subject = 'Mensaje desde el sitio web'; // Asunto del mail
+
+// Enviar mail
+mail($for, $subject, utf8_decode($message), $header);
+
+// Redireccionar una vez enviado
+header('Location:index.html');
 
 ?>
